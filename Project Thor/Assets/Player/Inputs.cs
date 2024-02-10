5bef5587d4c5e0f96dbc663b76322707db73c7e7
alias1233 <129236560+alias1233@users.noreply.github.com>
2024-02-10 02:30:01 -0500

    Replicated Inputs and Basic Movement

--- Project Thor/Assets/Player/Inputs.cs
+++ Project Thor/Assets/Player/Inputs.cs
@@ -1,24 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build;
+using Unity.Netcode;
 using UnityEngine;
 
-public struct Inputs
+public struct Inputs : INetworkSerializable
 {
     public int TimeStamp;
+    public Quaternion Rotation;
     public bool W;
     public bool S;
     public bool A;
     public bool D;
     public bool SpaceBar;
 
-    public Inputs(int timestamp, bool w, bool a, bool s, bool d, bool spacebar)
+    public Inputs(int timestamp, Quaternion rotation, bool w, bool a, bool s, bool d, bool spacebar)
     {
         TimeStamp = timestamp;
+        Rotation = rotation;
         W = w;
         A = a;
         S = s;
         D = d;
         SpaceBar = spacebar;
+    }
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        if (serializer.IsWriter)
+        {
+            FastBufferWriter fastBufferWriter = serializer.GetFastBufferWriter();
+
+            fastBufferWriter.WriteValueSafe(TimeStamp);
+            fastBufferWriter.WriteValueSafe(Rotation);
+            fastBufferWriter.WriteValueSafe(W);
+            fastBufferWriter.WriteValueSafe(A);
+            fastBufferWriter.WriteValueSafe(S);
+            fastBufferWriter.WriteValueSafe(D);
+            fastBufferWriter.WriteValueSafe(SpaceBar);
+        }
+
+        if (serializer.IsReader)
+        {
+            FastBufferReader fastBufferReader = serializer.GetFastBufferReader();
+
+            fastBufferReader.ReadValueSafe(out int timestamp);
+            fastBufferReader.ReadValueSafe(out Quaternion rotation);
+            fastBufferReader.ReadValueSafe(out bool w);
+            fastBufferReader.ReadValueSafe(out bool a);
+            fastBufferReader.ReadValueSafe(out bool s);
+            fastBufferReader.ReadValueSafe(out bool d);
+            fastBufferReader.ReadValueSafe(out bool spacebar);
+
+            TimeStamp = timestamp;
+            Rotation = rotation;
+            W = w;
+            A = a;
+            S = s;
+            D = d;
+            SpaceBar = spacebar;
+        }
     }
 }
--- Project Thor/Assets/Player/PlayerMovement.cs
+++ Project Thor/Assets/Player/PlayerMovement.cs
@@ -2,24 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class PlayerMovement : NetworkBehaviour
 {
+    public Transform Orientation;
+    public Rigidbody Rb;
+    public CapsuleCollider Collider;
+    public LayerMask layerMask;
+    public LayerMask WhatIsGround;
+
     public float moveSpeed = 3f;
+    public float JumpForce = 10f;
+    public float GroundFriction;
+    public float AirFriction;
+    public float Gravity;
 
-    public int ServerDelay = 2;
+    public int ServerDelay = 5;
 
+    private ClientRpcParams OwningClientID;
     private Dictionary<int, Inputs> InputsDictionary = new Dictionary<int, Inputs>();
     private Inputs CurrentInput;
     private int TimeStamp;
-    private Vector3 MoveDirection;
 
-    bool AutonomousProxy;
+    private float DeltaTime;
+
+    private int TotalTimes;
+    private int TotalTimeDifference;
+    private int LateInputsCount;
+    private int EarlyInputsCount;
+    private float LastTimeSentClientTimeCorrection;
+
+    bool bAutonomousProxy;
+    bool bHost;
+
+    private Vector3 MoveDirection;
+    private Vector3 Velocity;
+    private bool bIsGrounded;
+
+    private Bounds bounds;
+    private int MaxBounces = 5;
+    private float SkinWidth = 0.015f;
+    private float MaxSlopeAngle = 55;
 
     // Start is called before the first frame update
     void Start()
     {
-        AutonomousProxy = IsClient && IsOwner;
+        Rb = GetComponent<Rigidbody>();
+
+        bAutonomousProxy = IsClient && IsOwner;
+        bHost = IsServer && IsOwner;
+
+        if(IsServer && !IsOwner)
+        {
+            TimeStamp = -ServerDelay;
+
+            OwningClientID = new ClientRpcParams
+            {
+                Send = new ClientRpcSendParams
+                {
+                    TargetClientIds = new ulong[] { OwnerClientId }
+                }
+            };
+        }
     }
 
     // Update is called once per frame
@@ -27,20 +72,64 @@ public class PlayerMovement : NetworkBehaviour
     {
         TimeStamp++;
 
-        if (AutonomousProxy)
+        print(DeltaTime);
+
+        DeltaTime = Time.fixedDeltaTime;
+
+        if(bHost)
         {
-            AutonomousProxyTick();
+            HostTick();
+
+            ServerTickForAll();
+
+            return;
         }
 
-        if (IsServer && !IsOwner)
+        if (bAutonomousProxy)
         {
-            ServerTickForOtherPlayers();
+            AutonomousProxyTick();
+
+            return;
         }
 
         if (IsServer)
         {
-            ServerTickForAll();
+            ServerTickForOtherPlayers();
         }
+    }
+
+    void HostTick()
+    {
+        CurrentInput = new Inputs(TimeStamp, Orientation.transform.rotation, Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.S), Input.GetKey(KeyCode.D), Input.GetKey(KeyCode.Space));
+
+        HandleInputs(CurrentInput);
+
+        MovePlayer();
+    }
+
+    void ServerTickForOtherPlayers()
+    {
+        if(InputsDictionary.TryGetValue(TimeStamp, out Inputs inputs))
+        {
+            CurrentInput = inputs;
+        }
+
+        HandleInputs(CurrentInput);
+
+        MovePlayer();
+    }
+
+    void AutonomousProxyTick()
+    {
+        CurrentInput = new Inputs(TimeStamp, Orientation.transform.rotation, Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.S), Input.GetKey(KeyCode.D), Input.GetKey(KeyCode.Space));
+
+        InputsDictionary.Add(TimeStamp, CurrentInput);
+
+        SendInputsServerRpc(CurrentInput);
+
+        HandleInputs(CurrentInput);
+
+        MovePlayer();
     }
 
     void ServerTickForAll()
@@ -48,45 +137,210 @@ public class PlayerMovement : NetworkBehaviour
         ReplicatePositionClientRpc(transform.position);
     }
 
-    void ServerTickForOtherPlayers()
+    void MovePlayer()
     {
-        transform.position += MoveDirection * moveSpeed * Time.deltaTime;
+        bIsGrounded = Physics.Raycast(transform.position, Vector3.down, Collider.height * 0.5f + 0.15f, WhatIsGround);
+
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * (Collider.height * 0.5f + 0.1f));
+
+        Vector3 JumpVel = Vector3.zero;
+        Vector3 GravityVel = Gravity * Vector3.down;
+        float frictionmultiplier = 1 - AirFriction * DeltaTime;
+        Vector3 InputVel = (MoveDirection * moveSpeed) * (1 + AirFriction) * DeltaTime;
+
+        if (bIsGrounded)
+        {
+            GravityVel = Vector3.zero;
+
+            frictionmultiplier = 1 - GroundFriction * DeltaTime;
+
+            InputVel = (MoveDirection * moveSpeed) * (1 + GroundFriction) * DeltaTime;
+
+            Velocity.y = 0;
+
+            if (CurrentInput.SpaceBar)
+            {
+                JumpVel = Vector3.up * JumpForce;
+            }
+        }
+
+        Vector3 PreviousLocation = transform.position;
+
+        Vector3 Delta = (Velocity * frictionmultiplier + InputVel + JumpVel + GravityVel) * DeltaTime;
+
+        bounds = Collider.bounds;
+        bounds.Expand(-2 * SkinWidth);
+
+        transform.Translate(CollideAndSlide(transform.position, Delta, 0, Delta));
+
+        Velocity = (transform.position - PreviousLocation) / DeltaTime;
     }
 
-    void AutonomousProxyTick()
+    Vector3 CollideAndSlide(Vector3 Pos, Vector3 Vel, int depth, Vector3 VelInit)
     {
-        CurrentInput = new Inputs(TimeStamp, Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.S), Input.GetKey(KeyCode.D), Input.GetKey(KeyCode.Space));
-
-        InputsDictionary.Add(TimeStamp, CurrentInput);
-
-        SendInputsServerRpc(CurrentInput);
-
-        if (CurrentInput.W)
+        if(depth >= MaxBounces)
         {
-            MoveDirection.z = 1f;
+            return Vector3.zero;
         }
 
-        if (CurrentInput.A)
+        float dist = Vel.magnitude + SkinWidth;
+
+        Vector3 p1 = transform.position + Collider.center + Vector3.up * -Collider.height * 0.25F;
+        Vector3 p2 = p1 + Vector3.up * Collider.height * 0.5f;
+        RaycastHit hit;
+
+        if (Physics.CapsuleCast(
+            p1,
+            p2,
+            bounds.extents.x,
+            Vel.normalized,
+            out hit,
+            dist,
+            layerMask
+            ))
         {
-            MoveDirection.x = -1f;
+            Vector3 SnapToSurface = Vel.normalized * (hit.distance - SkinWidth);
+            Vector3 Leftover = Vel - SnapToSurface;
+            float Angle = Vector3.Angle(Vector3.up, hit.normal);
+
+            if(SnapToSurface.magnitude <= SkinWidth)
+            {
+                SnapToSurface = Vector3.zero;
+            }
+
+            /*
+            if(Angle <= MaxSlopeAngle)
+            {
+                Leftover = ProjectAndScale(Leftover, hit.normal);
+            }
+
+            else
+            {
+                float Scale = 1 - Vector3.Dot(
+                    new Vector3(hit.normal.x, 0, hit.normal.z).normalized,
+                    -new Vector3(VelInit.x, 0, VelInit.z).normalized
+                    );
+
+                if(bIsGrounded)
+                {
+                    Leftover = ProjectAndScale(
+                        new Vector3(Leftover.x, 0, Leftover.z),
+                        new Vector3(hit.normal.x, 0, hit.normal.z)
+                        ).normalized;
+
+                    Leftover *= Scale;
+                }
+
+                else
+                {
+                    Leftover = ProjectAndScale(Leftover, hit.normal) * Scale;
+                }
+            }
+            */
+
+            Leftover = ProjectAndScale(Leftover, hit.normal);
+
+            return SnapToSurface + CollideAndSlide(Pos + SnapToSurface, Leftover, depth + 1, VelInit);
         }
 
-        if (CurrentInput.S)
+        return Vel;
+    }
+
+    private Vector3 ProjectAndScale(Vector3 Vec, Vector3 Normal)
+    {
+        float Mag = Vec.magnitude;
+        Vec = Vector3.ProjectOnPlane(Vec, Normal).normalized;
+        Vec *= Mag;
+
+        return Vec;
+    }
+
+    void CheckClientTimeError(int clienttime)
+    {
+        if (Time.time - LastTimeSentClientTimeCorrection < 1)
         {
-            MoveDirection.z = -1f;
+            return;
         }
 
-        if (CurrentInput.D)
+        TotalTimes++;
+
+        int CorrectTime = TimeStamp + ServerDelay;
+
+        TotalTimeDifference = TotalTimeDifference + CorrectTime - clienttime;
+
+        if (clienttime < TimeStamp)
         {
-            MoveDirection.x = 1f;
+            LateInputsCount++;
         }
 
-        transform.position += MoveDirection * moveSpeed * Time.deltaTime;
+        if (clienttime > TimeStamp + 2 * ServerDelay)
+        {
+            EarlyInputsCount++;
+        }
+
+        if (LateInputsCount >= 3 || EarlyInputsCount >= 3 || Time.time - LastTimeSentClientTimeCorrection > 7)
+        {
+            LateInputsCount = 0;
+            EarlyInputsCount = 0;
+            LastTimeSentClientTimeCorrection = Time.time;
+
+            int timediff = TotalTimeDifference / TotalTimes;
+
+            if (timediff != 0)
+            {
+                SendClientTimeCorrectionClientRpc(timediff, OwningClientID);
+
+                print(TotalTimeDifference / TotalTimes);
+            }
+
+            TotalTimeDifference = 0;
+            TotalTimes = 0;
+        }
+    }
+
+    void HandleInputs(Inputs input)
+    {
+        MoveDirection = Vector3.zero;
+
+        Orientation.rotation = input.Rotation;
+
+        if (input.W)
+        {
+            MoveDirection += Orientation.transform.forward;
+        }
+
+        if (input.A)
+        {
+            MoveDirection -= Orientation.transform.right;
+        }
+
+        if (input.S)
+        {
+            MoveDirection -= Orientation.transform.forward;
+        }
+
+        if (input.D)
+        {
+            MoveDirection += Orientation.transform.right;
+        }
+
+        MoveDirection.Normalize();
+    }
+
+    [ClientRpc]
+    public void SendClientTimeCorrectionClientRpc(int timediff, ClientRpcParams clientRpcParams = default)
+    {
+        TimeStamp += timediff;
     }
 
     [ClientRpc]
     public void ReplicatePositionClientRpc(Vector3 position)
     {
+        if(bAutonomousProxy)
+        {
+            return;
+        }
+        
         transform.position = position;
     }
 
@@ -94,5 +348,7 @@ public class PlayerMovement : NetworkBehaviour
     public void SendInputsServerRpc(Inputs input)
     {
         InputsDictionary.Add(input.TimeStamp, input);
+
+        CheckClientTimeError(input.TimeStamp);
     }
 }
