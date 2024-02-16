e8f3e5deb4d1ef9e6a0d9f27d89f746fbbbd5bae
alias1233 <129236560+alias1233@users.noreply.github.com>
2024-02-15 22:17:35 -0500

    Started Removing Network Logic From Weapons
    Weapon Manager should handle network logic such as sending Rpcs to reduce the amount of network variables. The Weapon Manager will change the ActiveWeapon based on RPCs, and one RPC will handle shooting and stopping.

--- Project Thor/Assets/Player/Weapons/BaseWeapon.cs
+++ Project Thor/Assets/Player/Weapons/BaseWeapon.cs
@@ -1,79 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Netcode;
 
-public class BaseWeapon : NetworkBehaviour
+public class BaseWeapon : MonoBehaviour
 {
     public GameObject WeaponModel;
     public PlayerMovement PlayerMovementComponent;
-
-    private bool bIsActive;
-    private int TimeStamp;
 
     public int AmmoCount;
 
     public int FireCooldown1;
     public int FireCooldown2;
 
-    private bool IsShooting1;
-    private bool IsShooting2;
-
     public int Damage;
     public int Damage2;
 
-    private int LastTimeShot1;
-    private int LastTimeShot2;
-
-    private void FixedUpdate()
-    {
-        if(!bIsActive)
-        {
-            return;
-        }
-
-        TimeStamp++;
-
-        if (!IsOwner)
-        {
-            return;
-        }
-
-        if(Input.GetKey(KeyCode.Mouse0) || IsShooting1)
-        {
-            if(TimeStamp - LastTimeShot1 >= FireCooldown1)
-            {
-                LastTimeShot1 = TimeStamp;
-
-                Fire1();
-            }
-        }
-
-        else
-        {
-            StopFire1();
-        }
-
-        if (Input.GetKey(KeyCode.Mouse1) || IsShooting2)
-        {
-            if (TimeStamp - LastTimeShot2 >= FireCooldown2)
-            {
-                LastTimeShot2 = TimeStamp;
-
-                Fire2();
-            }
-        }
-
-        else
-        {
-            StopFire2();
-        }
-    }
-
     public virtual void ChangeActive(bool active)
     {
-        bIsActive = active;
-
         if(active)
         {
             WeaponModel.SetActive(true);
--- Project Thor/Assets/Player/Weapons/WeaponManager.cs
+++ Project Thor/Assets/Player/Weapons/WeaponManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class WeaponManager : MonoBehaviour
+public class WeaponManager : NetworkBehaviour
 {
     [SerializeField]
     private List<BaseWeapon> WeaponList;
 
     private BaseWeapon ActiveWeapon;
+
+    private int TimeStamp;
+
+    public bool IsShooting1;
+    public bool IsShooting2;
+
+    private int LastTimeShot1;
+    private int LastTimeShot2;
 
     private void Start()
     {
@@ -18,6 +27,71 @@ public class WeaponManager : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        TimeStamp++;
+
+        if (!IsOwner)
+        {
+            if(IsShooting1)
+            {
+                if (TimeStamp - LastTimeShot1 >= ActiveWeapon.FireCooldown1)
+                {
+                    LastTimeShot1 = TimeStamp;
+
+                    ActiveWeapon.Fire1();
+                }
+            }
+
+            else
+            {
+                ActiveWeapon.StopFire1();
+            }
+
+            if (IsShooting2)
+            {
+                if (TimeStamp - LastTimeShot2 >= ActiveWeapon.FireCooldown2)
+                {
+                    LastTimeShot2 = TimeStamp;
+
+                    ActiveWeapon.Fire2();
+                }
+            }
+
+            else
+            {
+                ActiveWeapon.StopFire2();
+            }
+
+            return;
+        }
+
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            if(TimeStamp - LastTimeShot1 >= ActiveWeapon.FireCooldown1)
+            {
+                LastTimeShot1 = TimeStamp;
+
+                ActiveWeapon.Fire1();
+            }
+        }
+
+        else
+        {
+            ActiveWeapon.StopFire1();
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (TimeStamp - LastTimeShot2 >= ActiveWeapon.FireCooldown2)
+            {
+                LastTimeShot2 = TimeStamp;
+
+                ActiveWeapon.Fire2();
+            }
+        }
+
+        else
+        {
+            ActiveWeapon.StopFire2();
+        }
     }
 }
